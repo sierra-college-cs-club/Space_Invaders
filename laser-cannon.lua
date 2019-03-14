303e2c63cc41e3cfad30f2be608eee9729f96017
McCrearyD <dyllanmccreary@gmail.com>
2019-03-14 15:51:50 -0700

    More comments & added buttons api require statement

--- laser-cannon.lua
+++ laser-cannon.lua
@@ -6,12 +6,14 @@ local lasercanon_body = display.newRect(display.contentCenterX, display.contentH
 
 -- create physics body with the rectangle. see: https://docs.coronalabs.com/api/library/physics/addBody.html
     -- Body Type: https://docs.coronalabs.com/api/type/Body/bodyType.html - Kinematic
-    -- Linear Velocity: https://docs.coronalabs.com/api/type/Body/setLinearVelocity.html
-
--- implement input event listener.
-    -- Touch: https://docs.coronalabs.com/api/event/touch/index.html
-    -- Keyboard: https://docs.coronalabs.com/api/event/key/index.html
-    -- Accelerometer: https://docs.coronalabs.com/api/event/accelerometer/index.html ** Advanced **
 
 -- implement a generalized movement function.
     -- should be able to be called from multiple event contexts.
+    -- Linear Velocity: https://docs.coronalabs.com/api/type/Body/setLinearVelocity.html
+
+local buttons = require('buttons')
+-- attach the movement function to it's corresponding button in this api.
+	-- Buttons.left_arrow
+	-- Buttons.right_arrow
+	-- Buttons.shoot
+	-- attach a 'touch' listener to each of these buttons.
