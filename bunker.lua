d626939765a9a787ca651abfa6fb8e7a58cc60be
McCrearyD <dyllanmccreary@gmail.com>
2019-03-14 15:52:19 -0700

    Rearranged imports

--- bunker.lua
+++ bunker.lua
@@ -1,2 +1,14 @@
 -- Stationary & partially blocks player / alien attacks.
 -- Slowly (and visually) destroyed with every hit.
+local createBunker = {}
+
+function createBunker:new(bunkNo, y)
+	-- body
+	local bunkers = {} 
+	for i = 1, bunkNo do
+		bunkers[i] = display.newRect(i*(screen.width/(bunkNo+1)), y, 10, 10 )
+	end
+	return bunkers
+end
+
+return createBunker
