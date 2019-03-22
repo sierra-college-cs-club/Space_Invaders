bf1e96e31bf1ac86c2362c30e4f25d7ae78d3932
abou123 <abou123@tuta.io>
2019-03-22 14:03:12 -0700

    Update bunker.lua
    added bunkers

--- bunker.lua
+++ bunker.lua
@@ -1,2 +1,14 @@
 -- Stationary & partially blocks player / alien attacks.
 -- Slowly (and visually) destroyed with every hit.
+
+local Bunker = {}
+
+function Bunker:new(x,y)
+	setmetatable(Bunker, self)
+	self.__index = self
+	
+	self.object = display.newRoundedRect( x, y, 40, 40, 5 )
+	return self
+end
+
+return Bunker
