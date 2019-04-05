1796fb912cc0bb1b38d7105559e7ff30dc4431b3
Dyllan McCreary <dmccreary@student.sierracollege.edu>
2019-04-05 14:49:47 -0700

    implemented laser collision and health variables

--- laser-cannon.lua
+++ laser-cannon.lua
@@ -3,13 +3,28 @@
 local Laser = require('laser')
 local glo = require('globals')
 local screen = glo.screen
-
+local PADDING = 0
 -- constants
 local SPEED = 7
 local dir = ""
 
 -- define body rectangle
+local hitsTaken = 0
+local maxHealth = 3
 local body = display.newRect(screen.xCenter, display.contentHeight + 20, 40, 20)
+body.isCannon = true
+physics.addBody(body,"kinematic")
+
+function body.onCollision(event)
+	
+	hitsTaken = hitsTaken + 1
+	print(hitsTaken)
+	if hitsTaken > maxHealth then
+		print("Dead")
+	end
+	
+
+end
 
 local function move(direction)
 	dir = direction
@@ -41,16 +56,19 @@ end
 
  function shootUp(event)
  	if event.phase == 'began' then
-		local l = Laser:new(body.x, body.y - body.height, "up")
+		local l = Laser:new(body.x, body.y - body.height - PADDING, "up")
 		l:fire()
 	end
 end
 --shootUp()
 function keyPress(event)
-	print (event.phase)
 	if event.phase == "down" then
-	
-		move(event.keyName)
+		if event.keyName == "up" or event.keyName == "space" then
+			local l = Laser:new(body.x, body.y - body.height - PADDING, "up")
+			l:fire()
+		else
+			move(event.keyName)
+		end
 	elseif event.phase == "up" then
 		move("")
 	end
