2d72cbd3b8bdcde648a90cbaaa94660fb4790745
Dyllan McCreary <dmccreary@student.sierracollege.edu>
2019-04-05 14:15:31 -0700

    removed shoot button

--- buttons.lua
+++ buttons.lua
@@ -38,19 +38,4 @@ Buttons.right_arrow = widget.newButton(
 Buttons.right_arrow.y = screen.yMax - Buttons.right_arrow.height / 2 - Buttons.yPadding
 Buttons.right_arrow.x = Buttons.right_arrow.width * 2
 
-
-
--- Define fire button
-Buttons.shoot = widget.newButton(
-    {
-        width = 50,
-        height = 50,
-        defaultFile = "assets/shoot.png",
-    }
-)
-
-Buttons.shoot.y = screen.yMax - Buttons.shoot.height / 2 - Buttons.yPadding
-Buttons.shoot.x = screen.xMax - Buttons.shoot.width / 2 - Buttons.xPadding
-
-
 return Buttons;
